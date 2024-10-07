c7cab5756ee65789e6c8368dc1969f5ed0ab0c18
Rahmanhusain <rahmanhusain899@gmail.com>
2024-10-07 19:09:54 +0530

    close to win commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['avatars.githubusercontent.com'],
+  images: {
+    remotePatterns: [
+      {
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**", // or specify a specific pathname if needed
       },
+    ],
+  },
 };
 
 export default nextConfig;
