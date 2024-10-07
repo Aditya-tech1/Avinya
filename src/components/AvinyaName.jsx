77b1ca2919abc3ec218bdf751880aea7a3220903
Aditya-tech1 <adityatech1m@gmail.com>
2024-10-07 22:59:30 +0530

    22:59

--- src/components/AvinyaName.jsx
+++ src/components/AvinyaName.jsx
@@ -6,8 +6,8 @@ const defaultOptions = {
   reverse: true, // reverse the tilt direction
   max: 35, // max tilt rotation (degrees)
   perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
-  scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
-  speed: 1000, // Speed of the enter/exit transition
+  scale: 1.05, // 2 = 200%, 1.5 = 150%, etc..
+  speed: 2000, // Speed of the enter/exit transition
   transition: true, // Set a transition on enter/exit.
   axis: null, // What axis should be disabled. Can be X or Y.
   reset: true, // If the tilt effect has to be reset on exit.
@@ -16,7 +16,7 @@ const defaultOptions = {
 
 function AvinyaName() {
   return (
-    <Tilt className="Tilt p-3" options={defaultOptions}>
+    <Tilt className="Tilt p-3 cursor-default" options={defaultOptions}>
       <h1 className="text-8xl py-4 [text-shadow:_7px_5px_16px_rgba(225,157,219,0.45)] allura-regular tracking-wider bg-gradient-to-r from-[#e63bdb] to-cyan-500 gradtext">
         <span className="kalam-regular">अ</span>VINYA 2024
       </h1>
