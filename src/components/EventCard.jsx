8c401049e9a8c04ea7e8cbbe379445d441760ae8
Aditya-tech1 <adityatech1m@gmail.com>
2024-10-08 14:58:50 +0530

    event phone

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -3,8 +3,8 @@ import logo from "../assets/logo.png";
 import Image from "next/image";
 function EventCard({data}) {
   return (
-    <div className="md:h-52 bg-gray-900 text-white p-5 rounded-lg flex flex-col md:flex-row items-center justify-center w-full ">
-      <div className="flex-shrink-0 h-full">
+    <div className="md:h-52 h-auto bg-gray-900 text-white p-5 rounded-lg flex flex-col md:flex-row items-center justify-center w-full ">
+      <div className="flex-shrink-0 md:h-full h-auto">
        {/*  <div className="h-full aspect-square bg-gray-700 rounded-lg"></div> */}
         <Image
           src={data.eventimage}
@@ -14,7 +14,7 @@ function EventCard({data}) {
           height={200}
         />
       </div>
-      <div className="ml-6 flex flex-col h-full grow">
+      <div className="md:ml-6 ml-0 mt-1 flex flex-col h-full grow">
         <h2 className="text-2xl font-bold mb-3">{data.eventname}</h2>
         <p className="mb-4 overflow-hidden line-clamp-4 text-sm text-gray-400 w-full grow">
           {data.eventdescription}
